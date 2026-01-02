a928766decf825d0c86ab02775fdeeb649020b72
Sadish Kumar Dash <59526333+TheSadish@users.noreply.github.com>
2026-01-02 04:15:30 +0000

    Store based on created date

--- services/aws_service.py
+++ services/aws_service.py
@@ -1,14 +1,23 @@
 import boto3 # type: ignore
+from datetime import datetime
 
 def get_bucket_info():
     s3_client = boto3.client('s3')
     bucket_json = s3_client.list_buckets()
-    print ("Error fetching details") if (bucket_json['ResponseMetadata']['HTTPStatusCode'] != 200 ) else print("Bucket Names:")
-    
-    # for bucket in bucket_json['Buckets']:
-    #     print(f"- {bucket['Name']}")
 
-    return bucket_json['Buckets']
+    if (bucket_json['ResponseMetadata']['HTTPStatusCode'] != 200 ):
+        print ("Error fetching details")
+
+    current_date = datetime.now()
+    old_buckets = []
+    new_buckets = []
+    for bucket in bucket_json['Buckets']:
+        bucket_created = bucket['CreationDate'].replace(tzinfo=None)
+        old_buckets.append(bucket["Name"])  if (current_date - bucket_created).days > 30 else new_buckets.append(bucket["Name"])   
+
+    return {
+        "Old_Buckets": old_buckets,
+        "New_Buckets": new_buckets
+    }
 
 print(get_bucket_info())
-
